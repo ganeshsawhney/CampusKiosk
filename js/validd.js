a840ad9f93d1b3a936b171242eb4888ce7d9ca73
ganeshsawhney <ganeshsawhney@gmail.com>
2016-03-20 14:58:27 +0530

    Javascript files

---
+++ js/validd.js
@@ -0,0 +1,26 @@
+function onlynum(evt) 
+		{
+			var theEvent = evt || window.event;
+			var key = theEvent.keyCode || theEvent.which;
+			key = String.fromCharCode( key );
+			var regex = /[0-9]|\./;
+			if( !regex.test(key) )
+			{
+			theEvent.returnValue = false;
+			if(theEvent.preventDefault) theEvent.preventDefault();
+			}
+		}  
+		function isspchar(evt)
+		{
+			var theEvent = evt || window.event;
+			var key = theEvent.keyCode || theEvent.which;
+			key = String.fromCharCode( key );
+			var regex = /[a-zA-Z]|\./;
+			if( !regex.test(key) )
+			{
+			theEvent.returnValue = false;
+			if(theEvent.preventDefault) theEvent.preventDefault();
+			}
+		}
+		
+		
